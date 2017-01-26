348916fba82af10c0d5e67b3385e972b8aac77a7
Addison Hu <huisaddison@users.noreply.github.com>
2017-01-26 01:46:26 -0500

    update publishconf

--- publishconf.py
+++ publishconf.py
@@ -18,6 +18,10 @@ CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
+PLUGIN_PATHS = ['../pelican-plugins']
+PLUGINS = ['render_math']
+
+
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
